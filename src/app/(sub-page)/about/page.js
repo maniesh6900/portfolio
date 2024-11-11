9edd1b30e523d34397c37abb22ac3a6229864e5f
maniesh6900 <kaj882626@gmail.com>
2024-11-11 10:00:41 +0530

    fixing errors in about page

--- src/app/(sub-page)/about/page.js
+++ src/app/(sub-page)/about/page.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import bg from "../../../../public/background/about-background.png";
-import RenderModel from "@/components";
-import AboutDetails from "@/components/about";
-import Staff from "@/Components/models/staff";
+// import RenderModel from "@/components/RenderModel.jsx";
+import AboutDetails from "@/components/about/index.jsx";
+// import Staff from "@/Components/models/staff.jsx";
 // import dynamic from "next/dynamic";
 // const HatModel = dynamic(() => import("@/components/models/HatModel"), {
 //   ssr: false,
@@ -21,11 +21,11 @@ export default function project() {
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
       />
 
-      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0 z-10">
+      {/* <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0 z-10">
         <RenderModel>
           <Staff />
         </RenderModel>
-      </div>
+      </div> */}
 
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
         <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
